9aaa7b2b5c28dfef4b43f0da1a3058a387d56207
Inês Soares <inesosoares.6@gmail.com>
2021-05-27 15:07:44 +0100

    spacial awareness update

--- Assets/Scripts/SetReferentialGestures.cs
+++ Assets/Scripts/SetReferentialGestures.cs
@@ -74,7 +74,7 @@ public class SetReferentialGestures : MonoBehaviour
 
                 // set referential orientation
                 lookVector = secondPoint - itemToPosition.transform.position;
-                lookRotation = Quaternion.LookRotation(lookVector, Vector3.right);
+                lookRotation = Quaternion.LookRotation(lookVector, Vector3.forward);
                 itemToPosition.transform.rotation = lookRotation;
 
                 break;
